cb4603a753685230d50c56b185b2bf607ec6f34f
Sumit Kumar <63787026+kisho1131@users.noreply.github.com>
2025-02-02 01:46:59 +0530

    Time: 59 ms (14.51%), Space: 61.8 MB (5.95%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,29 +1,29 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-            long max = -1L;
-            long sum = 0;
-            int start = 0;
-            int end = 0;
-            Map<Integer, Integer> freqMap = new HashMap<>();
-            while (end < nums.length) {
-            sum += nums[end];
+        int start = 0;
+        int end = 0;
+        long sum = 0;
+        long max = -1L;
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        
+        while(end  < nums.length) {
             freqMap.put(nums[end], freqMap.getOrDefault(nums[end], 0) + 1);
+            sum += nums[end];
             if (end - start + 1 < k) {
                 end++;
             }
-                else if (end - start + 1 == k) {
-                    if (freqMap.size() == k) {
+            else if(end - start + 1 == k) {
+                if(freqMap.size() == k) {
                     max = Math.max(max, sum);
-                    }
-                    sum -= nums[start];
-                    freqMap.put(nums[start], freqMap.get(nums[start]) - 1);
-                    if (freqMap.get(nums[start]) == 0) {
-                    freqMap.remove(nums[start]);
-                    }
-                    start ++;
-                    end ++;
                 }
+                sum-= nums[start];
+                freqMap.put(nums[start], freqMap.get(nums[start]) - 1);
+                if(freqMap.get(nums[start]) == 0)
+                    freqMap.remove(nums[start]);
+                start++;
+                end++;
             }
-            return max > 0 ? max : 0;
+        }
+        return max > 0 ? max : 0;
     }
 }
