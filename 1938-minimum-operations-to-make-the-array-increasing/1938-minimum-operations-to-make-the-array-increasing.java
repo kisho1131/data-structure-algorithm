207156ebd9b31c3239110b21410f1b34d8bb3f88
Sumit Kumar <63787026+kisho1131@users.noreply.github.com>
2025-09-17 23:16:02 +0530

    Time: 4 ms (73.24%), Space: 45.7 MB (5.58%) - LeetHub

--- 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.java
@@ -5,8 +5,7 @@ class Solution {
         int minOperation = 0;
         for(int i = 1; i < nums.length; i++) {
             if(nums[i] <= nums[i-1]) {
-                int delta = Math.abs(nums[i] - nums[i-1]) + 1;
-                minOperation += delta;
+                minOperation += nums[i-1] - nums[i] + 1;
                 nums[i] = nums[i-1] + 1;
             }
         }
